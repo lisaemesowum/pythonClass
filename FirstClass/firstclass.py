a514ffc0989347df83c1894bb170f492dc938919
Lisa Emesowum Amarachi <lisaemesowum8@gmail.com>
2026-05-18 13:05:05 +0100

    my first class python

---
+++ FirstClass/firstclass.py
@@ -0,0 +1,7 @@
+# print("Welcome to my first class in Python!")
+
+number1 = 30
+number2 = 60
+sum = number1 + number2
+print("The sum of", number1, "and", number2, "is", sum)
+print(f'sum = {sum}')
