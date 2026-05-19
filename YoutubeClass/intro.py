00890f29a630f1d80930ec6e63e85e064df49c29
Lisa Emesowum Amarachi <lisaemesowum8@gmail.com>
2026-05-19 22:21:46 +0100

    intro.py

---
+++ YoutubeClass/intro.py
@@ -0,0 +1,15 @@
+print("Hello😊,i am learning from youtube by 'Programming with Mosh',")
+print("*" *10) #multiply 10 times of the star *
+print("*" * 3)
+
+# sttle guide for pythone code in python you can read from 
+x=1
+y=2
+unit_price = x + y
+print(unit_price)
+
+
+
+
+
+
