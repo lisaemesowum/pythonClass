7f42add451f60df273db2732fc086c0124029564
Lisa Emesowum Amarachi <lisaemesowum8@gmail.com>
2026-05-22 14:16:54 +0100

    all folders added

--- Aptech_FirstClass/Task/pascal_Triangle/triangle.py
+++ Aptech_FirstClass/Task/pascal_Triangle/triangle.py
@@ -22,13 +22,9 @@ for i in range( triangle):
     print()
     
     
-    # anither method
-n = 5
-for i in range(n):
-    for j in range(n - i - 1):
-        print(" ", end="")
-    for k in range(i + 1):
-        print("* ", end="")
-    print()
+    # another method
+    
+
+    
          
      
